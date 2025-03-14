eac855aacf6869d71146910cdc307a9572f1c8b1
даня <165149039+domnichds@users.noreply.github.com>
2025-03-14 23:23:03 +0300

    Повторно обновить hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,9 +4,10 @@
 int
 main()
 {
-  std::string name;
+std::string name;
 #Username std::cin>> name;
 #Username get std::cout << "Hello world from " << name
                                        << std::endl;
 #Print return 0;
+
 }
